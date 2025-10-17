924c1e1965e6d648103f4a3656da40913f59dd49
Shivam Sen <shivamsen9644@gmail.com>
2025-10-17 18:34:55 +0530

    Abhi change

--- Backend/index.js
+++ Backend/index.js
@@ -27,6 +27,13 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://chatapp-yt-vu7b.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 const PORT = process.env.PORT || 3001;
 const URI = process.env.MONGODB_URI;
 
